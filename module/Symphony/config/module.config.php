c3553fdb88f68d9beb547c8d1491d8de4e5abeaa
Iuli Dercaci <iuli.dercaci@site-me.info>
2013-02-10 00:19:42 +0200

    added general module configuration, layout configuration, layout file, index controller, index view and error views

--- module/Symphony/config/module.config.php
+++ module/Symphony/config/module.config.php
@@ -6,12 +6,41 @@
 return array(
     'controllers' => array(
         'invokables' => array(
-            'Symphony\Controller\Symphony' => 'Symphony\Controller\SymphonyController',
+            'Symphony\Controller\Index' => 'Symphony\Controller\IndexController',
         ),
     ),
+    'router' => array(
+        'routes' => array(
+            'symphony' => array(
+                'type' => 'segment',
+                'options' => array(
+                    'route' => '/[:controller][/:action]',
+                    'constraints' => array(
+                        'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'action'     => '[a-zA-Z][a-zA-Z0-9_-]*'
+                    ),
+                    'defaults' => array(
+                        'controller'    => 'Symphony\Controller\Index',
+                        'action'        => 'index',
+                    )
+                )
+            )
+        )
+    ),
     'view_manager' => array(
+        'display_not_found_reason' => true,
+        'display_exceptions'       => true,
+        'doctype'                  => 'HTML5',
+        'not_found_template'       => 'error/404',
+        'exception_template'       => 'error/index',
+        'template_map' => array(
+            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+            'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
+            'error/404'               => __DIR__ . '/../view/error/404.phtml',
+            'error/index'             => __DIR__ . '/../view/error/index.phtml',
+        ),
         'template_path_stack' => array(
-            'symphony' => __DIR__ . '/../view',
+            __DIR__ . '/../view',
         ),
     ),
 );
---
+++ module/Symphony/src/Symphony/Controller/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+    /**
+     * @author: Iuli Dercaci <iuli.dercaci@site-me.info>
+     * @data: 2/10/13
+     */
+
+namespace Symphony\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+    class IndexController extends AbstractActionController
+    {
+        public function indexAction(){
+            echo 222;
+        }
+    }
